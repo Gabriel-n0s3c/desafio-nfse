50d8d936ae1342e0df41edd64d367de7a689c787
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2025-03-11 17:24:49 -0300

    feat: insere localePt para correta apresentação dos valores

--- nfse-estatico/src/app/app.module.ts
+++ nfse-estatico/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaCreditoComponent } from './lista-credito/lista-credito.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ToastModule } from 'primeng/toast'; 
+import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +23,16 @@ import { MessageService } from 'primeng/api';
     HttpClientModule,
     ToastModule
   ],
-  providers: [MessageService],
+  providers: [MessageService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
