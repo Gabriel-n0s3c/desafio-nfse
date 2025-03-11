24e27341e7ab993618bb148850c05fcb5c0061e9
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2025-03-11 01:22:37 -0300

    refactor: adiciona SETTER para a classe entidade Credito

--- nfse-api/src/main/java/com/desafio/nfse_api/feature/credito/entity/Credito.java
+++ nfse-api/src/main/java/com/desafio/nfse_api/feature/credito/entity/Credito.java
@@ -2,6 +2,7 @@ package com.desafio.nfse_api.feature.credito.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
