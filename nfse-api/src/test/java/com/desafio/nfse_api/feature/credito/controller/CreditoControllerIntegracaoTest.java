b19a4e0c2986f388e35335068241216d0b8de7a6
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2025-03-12 15:15:09 -0300

    test: ajusta importação teste de integração

--- nfse-api/src/test/java/com/desafio/nfse_api/feature/credito/controller/CreditoControllerIntegracaoTest.java
+++ nfse-api/src/test/java/com/desafio/nfse_api/feature/credito/controller/CreditoControllerIntegracaoTest.java
@@ -11,11 +11,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.KafkaContainer;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -42,6 +40,7 @@ class CreditoControllerIntegracaoTest {
     static void stopKafka() {
         kafkaContainer.stop();
     }
+
     @Autowired
     private MockMvc mockMvc;
 
